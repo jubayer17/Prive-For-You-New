c4717148f033f0980346b7518f7243e082ec33fd
jubayer17 <jubayer17@cse.pstu.ac.bd>
2026-01-07 11:33:10 +0600

    refactor: improve code formatting and structure in index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -34,114 +34,116 @@ import TeamMemberProfile from "./TeamMemberProfile";
 
 import TermsOfService from "./TermsOfService";
 
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 
 const PAGES = {
-    
-    BlogPost: BlogPost,
-    
-    Blogs: Blogs,
-    
-    Careers: Careers,
-    
-    FAQ: FAQ,
-    
-    GiftCards: GiftCards,
-    
-    Home: Home,
-    
-    Membership: Membership,
-    
-    OurStory: OurStory,
-    
-    Packages: Packages,
-    
-    Press: Press,
-    
-    PrivacyPolicy: PrivacyPolicy,
-    
-    ServiceDetail: ServiceDetail,
-    
-    Services: Services,
-    
-    Support: Support,
-    
-    Team: Team,
-    
-    TeamMemberProfile: TeamMemberProfile,
-    
-    TermsOfService: TermsOfService,
-    
-}
+  BlogPost: BlogPost,
+
+  Blogs: Blogs,
+
+  Careers: Careers,
+
+  FAQ: FAQ,
+
+  GiftCards: GiftCards,
+
+  Home: Home,
+
+  Membership: Membership,
+
+  OurStory: OurStory,
+
+  Packages: Packages,
+
+  Press: Press,
+
+  PrivacyPolicy: PrivacyPolicy,
+
+  ServiceDetail: ServiceDetail,
+
+  Services: Services,
+
+  Support: Support,
+
+  Team: Team,
+
+  TeamMemberProfile: TeamMemberProfile,
+
+  TermsOfService: TermsOfService,
+};
 
 function _getCurrentPage(url) {
-    if (url.endsWith('/')) {
-        url = url.slice(0, -1);
-    }
-    let urlLastPart = url.split('/').pop();
-    if (urlLastPart.includes('?')) {
-        urlLastPart = urlLastPart.split('?')[0];
-    }
+  if (url.endsWith("/")) {
+    url = url.slice(0, -1);
+  }
+  let urlLastPart = url.split("/").pop();
+  if (urlLastPart.includes("?")) {
+    urlLastPart = urlLastPart.split("?")[0];
+  }
 
-    const pageName = Object.keys(PAGES).find(page => page.toLowerCase() === urlLastPart.toLowerCase());
-    return pageName || Object.keys(PAGES)[0];
+  const pageName = Object.keys(PAGES).find(
+    (page) => page.toLowerCase() === urlLastPart.toLowerCase()
+  );
+  return pageName || Object.keys(PAGES)[0];
 }
 
 // Create a wrapper component that uses useLocation inside the Router context
 function PagesContent() {
-    const location = useLocation();
-    const currentPage = _getCurrentPage(location.pathname);
-    
-    return (
-        <Layout currentPageName={currentPage}>
-            <Routes>            
-                
-                    <Route path="/" element={<BlogPost />} />
-                
-                
-                <Route path="/BlogPost" element={<BlogPost />} />
-                
-                <Route path="/Blogs" element={<Blogs />} />
-                
-                <Route path="/Careers" element={<Careers />} />
-                
-                <Route path="/FAQ" element={<FAQ />} />
-                
-                <Route path="/GiftCards" element={<GiftCards />} />
-                
-                <Route path="/Home" element={<Home />} />
-                
-                <Route path="/Membership" element={<Membership />} />
-                
-                <Route path="/OurStory" element={<OurStory />} />
-                
-                <Route path="/Packages" element={<Packages />} />
-                
-                <Route path="/Press" element={<Press />} />
-                
-                <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
-                
-                <Route path="/ServiceDetail" element={<ServiceDetail />} />
-                
-                <Route path="/Services" element={<Services />} />
-                
-                <Route path="/Support" element={<Support />} />
-                
-                <Route path="/Team" element={<Team />} />
-                
-                <Route path="/TeamMemberProfile" element={<TeamMemberProfile />} />
-                
-                <Route path="/TermsOfService" element={<TermsOfService />} />
-                
-            </Routes>
-        </Layout>
-    );
+  const location = useLocation();
+  const currentPage = _getCurrentPage(location.pathname);
+
+  return (
+    <Layout currentPageName={currentPage}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/BlogPost" element={<BlogPost />} />
+
+        <Route path="/Blogs" element={<Blogs />} />
+
+        <Route path="/Careers" element={<Careers />} />
+
+        <Route path="/FAQ" element={<FAQ />} />
+
+        <Route path="/GiftCards" element={<GiftCards />} />
+
+        <Route path="/Home" element={<Home />} />
+
+        <Route path="/Membership" element={<Membership />} />
+
+        <Route path="/OurStory" element={<OurStory />} />
+
+        <Route path="/Packages" element={<Packages />} />
+
+        <Route path="/Press" element={<Press />} />
+
+        <Route path="/PrivacyPolicy" element={<PrivacyPolicy />} />
+
+        <Route path="/ServiceDetail" element={<ServiceDetail />} />
+
+        <Route path="/Services" element={<Services />} />
+
+        <Route path="/Support" element={<Support />} />
+
+        <Route path="/Team" element={<Team />} />
+
+        <Route path="/TeamMemberProfile" element={<TeamMemberProfile />} />
+
+        <Route path="/TermsOfService" element={<TermsOfService />} />
+      </Routes>
+    </Layout>
+  );
 }
 
 export default function Pages() {
-    return (
-        <Router>
-            <PagesContent />
-        </Router>
-    );
+  return (
+    <Router>
+      <PagesContent />
+    </Router>
+  );
 }
